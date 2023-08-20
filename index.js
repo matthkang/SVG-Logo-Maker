4f132b04d8d1a3ef10798397850f780be0da640f
matthew <matthkang@gmail.com>
2023-08-19 23:35:30 -0700

    circle class impl

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 // packages needed for application
 const inquirer = require('inquirer')
 const fs = require('fs')
-const Square = require('./lib/square.js')
 
 // create an array of questions for user input
 const questions = [
-    { message: 'What is the text of the SVG logo?', type: 'input', name: 'text' },
+    { message: 'What is the text of the SVG logo? (3 char limit)', type: 'input', name: 'text' },
     { message: 'What is the color of the text? (keyword or hexadecimal number)', type: 'input', name: 'textColor' },
     { message: 'Choose a shape for the logo:', type: 'list', choices: ['circle', 'triangle', 'square'], name: 'shape' },
     { message: 'What is the color of the shape? (keyword or hexadecimal number)', type: 'input', name: 'shapeColor' },
@@ -16,6 +15,7 @@ const questions = [
 inquirer.prompt(questions)
     .then((answers) => {
         if (answers.shape === 'square') {
+            const Square = require('./lib/square.js');
             const square = new Square();
             square.setText(answers.text);
             square.setTextColor(answers.textColor);
@@ -24,6 +24,17 @@ inquirer.prompt(questions)
 
             const svg = square.renderSvg();
             createSvg('./examples/square.svg', svg);
+        }
+        else if (answers.shape === 'circle'){
+            const Circle = require('./lib/circle.js');
+            const circle = new Circle();
+            circle.setText(answers.text);
+            circle.setTextColor(answers.textColor);
+            circle.setShape(answers.shape);
+            circle.setShapeColor(answers.shapeColor);
+
+            const svg = circle.renderSvg();
+            createSvg('./examples/circle.svg', svg);
         }
     })
 
